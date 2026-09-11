e8a35bb2469d29a2f28426845ebb45bb5d252eec
reeja shakya <reejashakya099@gmail.com>
2026-09-11 16:43:03 +0545

    Day 1: Create project foundation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # student-performance-analysis
 A python based student performance analysis project build step by step.
+
+Day 1 Progress:
+-Created the project folders
+-Added a student CSV dataset
+-Used lists, dictionaries, loops, and conditions
+-Calculated total and average marks
+-Classified student performance
+
+-The project includes a CSV file containing student names, ages, genders, and marks in Math, Science, and English.
+-This project will gradually develop into a student performance analysis system using Python and data analysis skills.
+
+
---
+++ src/main.py
@@ -0,0 +1,64 @@
+print("Student Performance Analysis System")
+print("-----------------------------------")
+print("Loading student data...")
+
+students = [
+    {
+        "name": "Salwin",
+        "age": 19,
+        "gender": "Male",
+        "math": 78,
+        "science": 82,
+        "english": 75
+    },
+    {
+        "name": "Sita",
+        "age": 20,
+        "gender": "Female",
+        "math": 88,
+        "science": 91,
+        "english": 85
+    },
+    {
+        "name": "Rohan",
+        "age": 19,
+        "gender": "Male",
+        "math": 65,
+        "science": 72,
+        "english": 68
+    }
+]
+
+for student in students:
+    print(student)
+    
+print("\nStudent Total Marks")
+print("-------------------")
+
+for student in students:
+    total = (student["math"]+ student["science"]+ student["english"])
+    print(student["name"], ":", total)
+    
+print("\nStudent Average Marks")
+print("---------------------")
+
+for student in students:
+    total = student["math"] + student["science"] + student["english"]
+    average = total / 3
+    print(student["name"], ":", average)
+    
+print("\nPerformance Result")
+print("------------------")
+
+for student in students:
+    total = student["math"] + student["science"] + student["english"]
+    average = total / 3
+
+    if average >= 80:
+        result = "Excellent"
+    elif average >= 60:
+        result = "Good"
+    else:
+        result = "Needs Improvement"
+
+    print(student["name"], ":", result)
